c9333d59325ce7e433e2df591068690d979cbc01
venky <m.venky499@gmail.com>
2022-01-25 09:49:44 +0530

    first commit

---
+++ food-cart/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import{RatingModule} from 'ng-starrating';
+import { AppRoutingModule } from './app-routing.module';
+import{FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { TagsComponent } from './tags/tags.component';
+import { FoodPageComponent } from './food-page/food-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    TagsComponent,
+    FoodPageComponent,
+    CartPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RatingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
